934c56cb8b0fcf4f1b32d09fc897ed0cf84ac5c4
Brian Gass <briangass3@gmail.com>
2020-04-13 16:37:12 -0400

    Implement databinding for components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
   selector: "app-header",
@@ -6,4 +6,9 @@ import { Component } from "@angular/core";
 })
 export class HeaderComponent {
   collapsed = true;
+  @Output() sectionSelected = new EventEmitter<string>();
+
+  onSelect(section: string) {
+    this.sectionSelected.emit(section);
+  }
 }
