69cd103189ea3e79d454c1fe30c2bc0e95937d3c
Brian Gass <briangass3@gmail.com>
2020-04-17 17:04:32 -0400

    Enable add ingredients to shoppinglist from recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,22 +1,33 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Recipe } from "./recipe.model";
+import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe(
       "Testipe",
       "Easy and delish!",
-      "https://storage.needpix.com/rsynced_images/potato-soup-2152265_1280.jpg"
+      "https://storage.needpix.com/rsynced_images/potato-soup-2152265_1280.jpg",
+      [new Ingredient("Mangos", 2), new Ingredient("Milk", 1)]
     ),
     new Recipe(
       "Groupar",
       "Served with energy and dust.",
-      "https://storage.needpix.com/rsynced_images/potato-soup-2152265_1280.jpg"
+      "https://storage.needpix.com/rsynced_images/potato-soup-2152265_1280.jpg",
+      [new Ingredient("Fish sticks", 1), new Ingredient("Noodles", 4)]
     ),
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
