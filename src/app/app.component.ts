31f49dcbd25bc13d50811ab2a2a78c7e4d156382
Brian Gass <briangass3@gmail.com>
2020-04-27 14:44:28 -0400

    Update EventEmitters to Subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.css"]
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  loadedSelection = "recipe";
-  onNavigate(section: string) {
-    this.loadedSelection = section;
-  }
-}
+export class AppComponent {}
