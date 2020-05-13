a41c9e78f12bea8c291498fbce7b34e06ec43549
Brian Gass <briangass3@gmail.com>
2020-05-13 09:20:58 -0400

    Create and store user data

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,25 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { catchError } from "rxjs/operators";
-import { throwError } from "rxjs";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { catchError, tap } from "rxjs/operators";
+import { throwError, Subject } from "rxjs";
+import { User } from "./user.model";
 
-interface AuthResponseData {
+export interface AuthResponseData {
   kind: string;
   idToken: string;
   email: string;
   refreshToken: string;
   expiresIn: string;
   localId: string;
+  registered?: boolean;
 }
 
 @Injectable({ providedIn: "root" })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
+
   singup(email: string, password: string) {
     return this.http
       .post<AuthResponseData>(
@@ -22,18 +27,59 @@ export class AuthService {
         { email: email, password: password, returnSecureToken: true }
       )
       .pipe(
-        catchError((errorRes) => {
-          let errorMessage = "An nknown error occurred.";
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorRes.error.error.message) {
-            case "EMAIL_EXISTS":
-              errorMessage =
-                "This email is already associated with an account.";
-          }
-          return throwError(errorMessage);
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
         })
       );
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD4I_7GPk9LMCb2lf84Vfgd9Sw7noc8-gQ",
+        { email: email, password: password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuth(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  handleAuth(email: string, userId: string, token: string, expiresIn: number) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
+  }
+
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMessage = "An unknown error occurred.";
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorRes.error.error.message) {
+      case "EMAIL_EXISTS":
+        errorMessage = "This email is already associated with an account.";
+        break;
+      case "EMAIL_NOT_FOUND":
+        errorMessage = "There is no user associated with this email.";
+        break;
+      case "INVALID_PASSWORD":
+        errorMessage = "The password is not valid.";
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
