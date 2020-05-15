a02606eda305b87c0c7194a722401f25cb882538
Brian Gass <briangass3@gmail.com>
2020-05-15 12:41:34 -0400

    Separate recipes feature module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesComponent } from "./recipes.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeLandingComponent } from "./recipe-landing/recipe-landing.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipesRoutingModule } from "./recipes.routing.module";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeLandingComponent,
+    RecipeEditComponent,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule,
+    RecipesRoutingModule,
+  ],
+})
+export class RecipesModule {}
