8d408573de2eb78755052d0e55be2f5eea3d6684
Brian Gass <briangass3@gmail.com>
2020-05-15 13:49:10 -0400

    Refactor shared, core and auth modules

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { AlertComponent } from "./alert/alert.component";
+import { LoadingSpinnerComponent } from "./loading-spinner/loading-spinner.component";
+import { DropDownDirective } from "./dropdown.directive";
+
+@NgModule({
+  declarations: [AlertComponent, LoadingSpinnerComponent, DropDownDirective],
+  imports: [CommonModule],
+  exports: [
+    AlertComponent,
+    LoadingSpinnerComponent,
+    DropDownDirective,
+    CommonModule,
+  ],
+})
+export class SharedModule {}
--- src/app/shopping-list/shopping-list-module.ts
+++ src/app/shopping-list/shopping-list-module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
+import { SharedModule } from "../shared/shared.module";
 import { ShoppingListComponent } from "./shopping-list.component";
 import { ShoppingListEditComponent } from "./shopping-list-edit/shopping-list-edit.component";
 
 @NgModule({
   declarations: [ShoppingListComponent, ShoppingListEditComponent],
   imports: [
-    CommonModule,
     FormsModule,
     RouterModule.forChild([
       { path: "shopping-list", component: ShoppingListComponent },
     ]),
+    SharedModule,
   ],
 })
 export class ShoppingListModule {}
