b7ed0f6dcb46a192dacee65d0a49f999828de76a
Brian Gass <briangass3@gmail.com>
2020-06-11 12:56:26 -0400

    Refactor from services to ngrx

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -5,10 +5,8 @@ import {
   ViewChild,
 } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { Router } from "@angular/router";
 import { Store } from "@ngrx/store";
-import { Observable, Subscription } from "rxjs";
-import { AuthService, AuthResponseData } from "./auth.service";
+import { Subscription } from "rxjs";
 import { PlaceholderDirective } from "../shared/placeholder/placeholder.directive";
 import * as fromApp from "../store/app.reducer";
 import * as AuthActions from "./store/auth.actions";
@@ -29,8 +27,6 @@ export class AuthComponent implements OnInit {
   private storeSub: Subscription;
 
   constructor(
-    private authService: AuthService,
-    private router: Router,
     private componentFactoryResolver: ComponentFactoryResolver,
     private store: Store<fromApp.AppState>
   ) {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
-const firebaseApiKey = "AIzaSyD4I_7GPk9LMCb2lf84Vfgd9Sw7noc8-gQ";
-const firebaseUrl = "https://ng-recipe-app-e3a23.firebaseio.com/";
-// const firebaseApiKey = "YOUR_FIREBASE_API_KEY";
-// const firebaseUrl = "YOUR_FIREBASE_PROJECT.firebaseio.com/";
+const firebaseApiKey = "YOUR_FIREBASE_API_KEY";
+const firebaseUrl = "YOUR_FIREBASE_PROJECT.firebaseio.com/";
 export const environment = {
   production: true,
   firebaseApiKey,
